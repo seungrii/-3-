085fd3ff9206f8f62a5af642f2bd9727d5b56f69
seungrii <choevictory@gmail.com>
2023-01-10 20:59:11 +0900

    board84 생성

---
+++ board84/src/main/java/com/seungri/board84/Board84Application.java
@@ -0,0 +1,13 @@
+package com.seungri.board84;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board84Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Board84Application.class, args);
+	}
+
+}
---
+++ board84/src/test/java/com/seungri/board84/Board84ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seungri.board84;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Board84ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
